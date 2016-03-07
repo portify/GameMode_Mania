db7c0602ab7b3462045ac0061e22e9bdb5e1ae43
Alex Noir <Varsash@Gmail.com>
2016-03-07 23:00:03 +0300

    rename music files to have "mania_" prefix for ease of updates because Plastiware demands it

--- src/sound.cs
+++ src/sound.cs
@@ -24,181 +24,181 @@ datablock AudioProfile(ManiaCompleteYou)
 
 datablock AudioProfile(ManiaIntroMusic)
 {
-	fileName = "Add-Ons/GameMode_Mania/res/intro.ogg";
+	fileName = "Add-Ons/GameMode_Mania/res/mania_intro.ogg";
 	description = ManiaMusicDescription;
 	preload = 1;
 };
 
 datablock AudioProfile(ManiaGameEndMusic)
 {
-	fileName = "Add-Ons/GameMode_Mania/res/game_end.ogg";
+	fileName = "Add-Ons/GameMode_Mania/res/mania_game_end.ogg";
 	description = ManiaMusicDescription;
 	preload = 1;
 };
 
 datablock AudioProfile(ManiaGameStart1Music)
 {
-	fileName = "Add-Ons/GameMode_Mania/res/game_start_1.ogg";
+	fileName = "Add-Ons/GameMode_Mania/res/mania_game_start_1.ogg";
 	description = ManiaMusicDescription;
 	preload = 1;
 };
 
 datablock AudioProfile(ManiaGameStart2Music)
 {
-	fileName = "Add-Ons/GameMode_Mania/res/game_start_2.ogg";
+	fileName = "Add-Ons/GameMode_Mania/res/mania_game_start_2.ogg";
 	description = ManiaMusicDescription;
 	preload = 1;
 };
 
 datablock AudioProfile(ManiaGameBossMusic)
 {
-	fileName = "Add-Ons/GameMode_Mania/res/game_boss.ogg";
+	fileName = "Add-Ons/GameMode_Mania/res/mania_game_boss.ogg";
 	description = ManiaMusicDescription;
 	preload = 1;
 };
 
 datablock AudioProfile(ManiaGameWinMusic)
 {
-	fileName = "Add-Ons/GameMode_Mania/res/game_win.ogg";
+	fileName = "Add-Ons/GameMode_Mania/res/mania_game_win.ogg";
 	description = ManiaMusicDescription;
 	preload = 1;
 };
 
 datablock AudioProfile(ManiaGameFailMusic)
 {
-	fileName = "Add-Ons/GameMode_Mania/res/game_fail.ogg";
+	fileName = "Add-Ons/GameMode_Mania/res/mania_game_fail.ogg";
 	description = ManiaMusicDescription;
 	preload = 1;
 };
 
 datablock AudioProfile(ManiaGameSpeedUpMusic)
 {
-	fileName = "Add-Ons/GameMode_Mania/res/game_speedup.ogg";
+	fileName = "Add-Ons/GameMode_Mania/res/mania_game_speedup.ogg";
 	description = ManiaMusicDescription;
 	preload = 1;
 };
 
 datablock AudioProfile(ManiaGameSpecialRoundMusic)
 {
-	fileName = "Add-Ons/GameMode_Mania/res/special_round.ogg";
+	fileName = "Add-Ons/GameMode_Mania/res/mania_special_round.ogg";
 	description = ManiaMusicDescription;
 	preload = 1;
 };
 
 datablock AudioProfile(ManiaChipMusic)
 {
-	fileName = "Add-Ons/GameMode_Mania/res/chip.ogg";
+	fileName = "Add-Ons/GameMode_Mania/res/mania_chip.ogg";
 	description = ManiaMusicDescription;
 	preload = 1;
 };
 
 datablock AudioProfile(ManiaHoedownMusic)
 {
-	fileName = "Add-Ons/GameMode_Mania/res/hoedown.ogg";
+	fileName = "Add-Ons/GameMode_Mania/res/mania_hoedown.ogg";
 	description = ManiaMusicDescription;
 	preload = 1;
 };
 
 datablock AudioProfile(ManiaJazzMusic)
 {
-	fileName = "Add-Ons/GameMode_Mania/res/jazz.ogg";
+	fileName = "Add-Ons/GameMode_Mania/res/mania_jazz.ogg";
 	description = ManiaMusicDescription;
 	preload = 1;
 };
 
 datablock AudioProfile(ManiaMetalMusic)
 {
-	fileName = "Add-Ons/GameMode_Mania/res/metal.ogg";
+	fileName = "Add-Ons/GameMode_Mania/res/mania_metal.ogg";
 	description = ManiaMusicDescription;
 	preload = 1;
 };
 
 datablock AudioProfile(ManiaPanicMusic)
 {
-	fileName = "Add-Ons/GameMode_Mania/res/panic.ogg";
+	fileName = "Add-Ons/GameMode_Mania/res/mania_panic.ogg";
 	description = ManiaMusicDescription;
 	preload = 1;
 };
 
 datablock AudioProfile(ManiaScotsMusic)
 {
-	fileName = "Add-Ons/GameMode_Mania/res/scots.ogg";
+	fileName = "Add-Ons/GameMode_Mania/res/mania_scots.ogg";
 	description = ManiaMusicDescription;
 	preload = 1;
 };
 
 datablock AudioProfile(ManiaWeirdMusic)
 {
-	fileName = "Add-Ons/GameMode_Mania/res/weird.ogg";
+	fileName = "Add-Ons/GameMode_Mania/res/mania_weird.ogg";
 	description = ManiaMusicDescription;
 	preload = 1;
 };
 
 datablock AudioProfile(ManiaFancyMusic)
 {
-	fileName = "Add-Ons/GameMode_Mania/res/fancy.ogg";
+	fileName = "Add-Ons/GameMode_Mania/res/mania_fancy.ogg";
 	description = ManiaMusicDescription;
 	preload = 1;
 };
 
 datablock AudioProfile(ManiaFunkyMusic)
 {
-	fileName = "Add-Ons/GameMode_Mania/res/funky.ogg";
+	fileName = "Add-Ons/GameMode_Mania/res/mania_funky.ogg";
 	description = ManiaMusicDescription;
 	preload = 1;
 };
 
 datablock AudioProfile(ManiaRuskieMusic)
 {
-	fileName = "Add-Ons/GameMode_Mania/res/ruskie.ogg";
+	fileName = "Add-Ons/GameMode_Mania/res/mania_ruskie.ogg";
 	description = ManiaMusicDescription;
 	preload = 1;
 };
 
 datablock AudioProfile(ManiaSpaceMusic)
 {
-	fileName = "Add-Ons/GameMode_Mania/res/space.ogg";
+	fileName = "Add-Ons/GameMode_Mania/res/mania_space.ogg";
 	description = ManiaMusicDescription;
 	preload = 1;
 };
 
 datablock AudioProfile(ManiaPipesMusic)
 {
-	fileName = "Add-Ons/GameMode_Mania/res/pipes.ogg";
+	fileName = "Add-Ons/GameMode_Mania/res/mania_pipes.ogg";
 	description = ManiaMusicDescription;
 	preload = 1;
 };
 
 datablock AudioProfile(ManiaWesternMusic)
 {
-	fileName = "Add-Ons/GameMode_Mania/res/western.ogg";
+	fileName = "Add-Ons/GameMode_Mania/res/mania_western.ogg";
 	description = ManiaMusicDescription;
 	preload = 1;
 };
 
 datablock AudioProfile(ManiaJazzLongMusic)
 {
-	fileName = "Add-Ons/GameMode_Mania/res/jazz_long.ogg";
+	fileName = "Add-Ons/GameMode_Mania/res/mania_jazz_long.ogg";
 	description = ManiaMusicDescription;
 	preload = 1;
 };
 datablock AudioProfile(ManiaMetalLongMusic)
 {
-	fileName = "Add-Ons/GameMode_Mania/res/metal_long.ogg";
+	fileName = "Add-Ons/GameMode_Mania/res/mania_metal_long.ogg";
 	description = ManiaMusicDescription;
 	preload = 1;
 };
 
 datablock AudioProfile(ManiaBossWestMusic)
 {
-	fileName = "Add-Ons/GameMode_Mania/res/boss_west.ogg";
+	fileName = "Add-Ons/GameMode_Mania/res/mania_boss_west.ogg";
 	description = ManiaLoopingMusicDescription;
 	preload = 1;
 };
 
 datablock AudioProfile(ManiaBossChipMusic)
 {
-	fileName = "Add-Ons/GameMode_Mania/res/boss_chip.ogg";
+	fileName = "Add-Ons/GameMode_Mania/res/mania_boss_chip.ogg";
 	description = ManiaLoopingMusicDescription;
 	preload = 1;
 };
